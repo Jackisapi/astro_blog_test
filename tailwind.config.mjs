6bb3d9fe529bd3a06a906be0c3007ca5b869e195
jackisapi <getphyk3d@gmail.com>
2024-03-28 20:53:25 -0600

    Made layout component
    thanks @sexnine lol

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,25 +12,27 @@ module.exports = {
 			lg: '976px',
 			xl: '1440px',
 		},
-		colors: {
-			'blue': '#1fb6ff',
-			'pink': '#ff49db',
-			'orange': '#ff7849',
-			'green': '#13ce66',
-			'gray-dark': '#273444',
-			'gray': '#8492a6',
-			'gray-light': '#d3dce6',
-			'coral-pink': '#f07f75',
-			'pastel-purple' : '#c3b1e1',
-			'better-grey' :  '#313338',
-			'salmon' : '#f26967'
-		},
+
 		fontFamily: {
 			sans: ['Graphik', 'sans-serif'],
 			serif: ['Merriweather', 'serif'],
 		},
 
 		extend: {
+			colors: {
+				'blue': '#1fb6ff',
+				'pink': '#ff49db',
+				'orange': '#ff7849',
+				'green': '#13ce66',
+				'gray-dark': '#273444',
+				'gray': '#8492a6',
+				'gray-light': '#d3dce6',
+				'coral-pink': '#f07f75',
+				'pastel-purple' : '#c3b1e1',
+				'better-grey' :  '#313338',
+				'salmon' : '#f26967'
+
+			},
 			spacing: {
 				'128': '32rem',
 				'144': '36rem',
