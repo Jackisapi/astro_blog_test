c9ce09c1dd07823fe711322d0184e70b1025f5ec
jackisapi <getphyk3d@gmail.com>
2024-03-29 16:03:48 -0600

    Messed with styling and whatnot

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  corePlugins: {
-    preflight: false,
-  },
+  corePlugins: {},
 
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
